a74c7d45c237413f211ccee71295ee499346977c
Tautvydas Petkus <tautvydas.petkus@telesofas.com>
2022-04-14 21:30:18 +0300

    Base application for our project
    A basic application to be wrapped in Python project structure

---
+++ app.py
@@ -0,0 +1,20 @@
+import click
+from flask import Flask
+from flask_restx import Api, Resource
+
+app = Flask(__name__)
+api = Api(app)
+
+
+@api.route('/hello', defaults={'obj': 'World'})
+@api.route('/hello/<obj>')
+class HelloWorld(Resource):
+    def get(self, obj):
+        return {'hello': obj}
+
+
+# custom flask cli command
+@app.cli.command()
+def hello():
+    """Just say hello."""
+    click.echo('Hello, Command-Line!')
