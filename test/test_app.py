2bf0e7c95b3171a3b614bb58b9daa72fc0da9200
Tautvydas Petkus <tautvydas.petkus@telesofas.com>
2022-04-17 19:41:54 +0300

    Testing dependencies added
    First development dependency and structure for unit tests for the project

---
+++ test/test_app.py
@@ -0,0 +1,8 @@
+from my_app import app
+
+
+def test_app():
+    with app.app.test_client() as client:
+        response = client.get(f'/hello')
+        assert response.status_code == 200
+        assert response.get_json() == {'hello': 'World'}
